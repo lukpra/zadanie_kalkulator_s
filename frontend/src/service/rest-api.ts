273e1c390e1c0f2dadeb38f8cb85cebc87ba0174
Lukasz Prazynski <lukasz.prazynski@gmail.com>
2019-06-18 02:09:12 +0200

    Fix frontend path

--- frontend/src/service/rest-api.ts
+++ frontend/src/service/rest-api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export class RestAPI {
-    public static URL: string = 'ENDPOINT_FULL_URL';
+    public static URL: string = 'http://localhost:8080/salary/calculate';
 
     public static async getNetto(dailyWage: number, countryCode: string) {
         return await axios.get(RestAPI.URL, {
